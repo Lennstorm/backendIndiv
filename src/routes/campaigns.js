d8695f05b730b639d56409fa529eb3b5e0d1c105
Lennstorm <4824136@gmail.com>
2024-06-14 16:07:46 +0200

    flyttat grejer till controller

--- src/routes/campaigns.js
+++ src/routes/campaigns.js
@@ -1,63 +1,25 @@
 import express from "express";
-import { allowAdmin } from "../../src/middleware/allowAdmin.js";
+import { allowAdmin } from "../middleware/allowAdmin.js";
 import { validateCampProds } from "../middleware/validateCampProds.js";
-import { createCampaign, getAllCampaigns, updateCampaign, deleteCampaign } from "../services/campaign.js";
+import {
+    createCampaignController,
+    getAllCampaignsController,
+    updateCampaignController,
+    deleteCampaignController
+} from "../controllers/campaignController.js";
 
-const router = express.Router()
+const router = express.Router();
 
 // POST ny kampanj ***** -- ENDAST ADMIN -- *****
-router.post("/", allowAdmin, validateCampProds, async (req, res) => {
-    const newCampaign = req.body;
-    try {
-        const createdCampaign = await createCampaign(newCampaign);
-        res.status(201).json({ message: "Kampanjen har skapats" });
-    }   catch (error) {
-        res.status(500).json({ message: "Internt serverfel" });
-    }
-});
+router.post("/", allowAdmin, validateCampProds, createCampaignController);
 
 // GET alla kampanjer
-router.get("/", async (req,res) => {
-    try {
-        const campaigns = await getAllCampaigns();
-        if (campaigns.length === 0) {
-            return res.status(200).json({ message: "Inga kampanjer finns för tillfället." });
-        }
-        res.status(200).json(campaigns);
-    }   catch (error) {
-        res.status(500).json({ message: "Internt serverfel" });
-    }
-});
+router.get("/", getAllCampaignsController);
 
 // PUT uppdatera kampanj ***** -- ENDAST ADMIN -- *****
-router.put("/:id", allowAdmin, validateCampProds, async (req, res) => {
-    const id = req.params.id;
-    const updatedCampaign = req.body;
-    try {
-        await updateCampaign(id, updatedCampaign);
-        res.json({ message: "Kampanjen uppdaterad!"});
-    }   catch (error) {
-        if (error.status === 404) {
-            res.status(404).json({ message: "Kampanjen hittas inte!" });
-        } else {
-            res.status(500).json({ message: "Internt serverfel"})
-        }
-    }
-});
+router.put("/:id", allowAdmin, validateCampProds, updateCampaignController);
 
 // DELETE kampanj ***** -- ENDAST ADMIN -- *****
-router.delete("/:id", allowAdmin, async (req, res) => {
-    const id = req.params.id;
-    try {
-        await deleteCampaign(id);
-        res.json({ message: "Kampanjen raderad!"});
-    }   catch(error) {
-        if (error.status === 404) {
-            res.status(404).json({ message: "Kampanjen hittas inte!"});
-        }   else {
-            res.status(500).json({ message: "Internt serverfel"})
-        }
-    }
-});
+router.delete("/:id", allowAdmin, deleteCampaignController);
 
 export default router;
